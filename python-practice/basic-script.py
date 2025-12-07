d34e99f8b27dd01a946bdd3c70d7de921599a36d
kubota-a <code199312@gmail.com>
2025-12-07 23:04:00 +0900

    「数値と演算」の演習
    変数、四則演算、print関数のアウトプットをするため

--- python-practice/basic-script.py
+++ python-practice/basic-script.py
@@ -1,1 +1,17 @@
 print("こんにちは！")
+
+# 演習問題　
+# 変数aには3、変数bには-6が格納されています。
+# 変数plus、minus、multiply、divideにaとbの足し算、引き算、掛け算、割り算の計算結果を格納して、すべて表示してください。
+a = 3
+b = -6
+# 変数plusに足し算の結果を格納
+plus = a + b
+# 変数minusに引き算の結果を格納
+minus = a - b
+# 変数multiplyに掛け算の結果を格納
+multiply = a * b
+# 変数divideに割り算の結果を格納
+divide = a / b
+# 各計算結果の表示
+print (plus, minus, multiply, divide) 
