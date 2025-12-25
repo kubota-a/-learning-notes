4e4683ccba05b6aa0c94f40f6db2fe750a5efb3d
kubota-a <code199312@gmail.com>
2025-12-25 21:32:16 +0900

    udemy問題集の1～7問目の復習を追加

--- python-practice/basic-exercises/udemy100_1225.py
+++ python-practice/basic-exercises/udemy100_1225.py
@@ -19,3 +19,59 @@ for i in range(1, 41):
     # 「"3" in i_str」だけでいいのに「 "3" in i_str == True」と書いて失敗　inはそれだけで完成品の条件式なので== Trueは不要！！
         three.append(i)
 print(f'作成したリスト：{three}')
+
+# 変数i_strを省くともっと簡素
+three = []
+for i in range(1, 41):
+    if i % 3 == 0 and "3" in str(i):
+                          #  str(i)これ、いったん他の変数に入れないとiの型が変わっちゃう…と心配していたのだが、i = str(i)にしてないから平気
+        three.append(i)
+print(f'作成したリスト：{three}')
+
+print("-" * 20)# ----------
+# 第1章の復習　※問題文は省略！
+
+# ◎問題1
+a = 7
+b = 3
+print(f'{a} + {b} = {a+b}')
+print(f'{a} - {b} = {a-b}')
+print(f'{a} × {b} = {a*b}')
+print(f'{a} ÷ {b} = {a//b} 余り {a%b}')
+print(f'{a} ÷ {b} = {a/b}')
+
+print("-" * 20)# ----------
+# ×問題2・×問題3　➡　Googlecolabで実演
+
+# ◎問題4
+for i in range(1, 31):
+    print(i) 
+
+# 問題5  while文省略
+
+print("-" * 20)# ----------
+# ◎問題6
+for t in range(1, 31):
+    if t % 3 == 0:
+        print(t)
+
+print("-" * 20)# ----------
+# ◎問題7
+for n in range(1, 31):
+    if n % 15 == 0:
+        print("fizzbuzz")
+    elif n % 3 == 0:
+        print("fizz")
+    elif n % 5 == 0:
+        print("buzz")
+    else:
+        print(n)
+
+print("-" * 20)# ----------
+# 問題8・9・10は次回復習
+
+
+
+
+
+
