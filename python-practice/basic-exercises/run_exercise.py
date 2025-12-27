dd22440322d1ce89178dc40c7992297e0332e9d6
kubota-a <code199312@gmail.com>
2025-12-27 22:03:28 +0900

    udemy100問中22～23問目を追加（夜）

--- python-practice/basic-exercises/run_exercise.py
+++ python-practice/basic-exercises/run_exercise.py
@@ -1,7 +1,10 @@
-# 別解　新たな文字列を作らずに、もとの文字列からダイレクトに母音を削除する方法
-word = input("文字列を入力してください > ")
-vowel_list = ["a", "i", "u", "e", "o"]
-for w in vowel_list:    # 母音の入ったリストvowel_listをループの対象とする
-    word = word.replace(w, "")
-    print(word)
-print(f'作成した文字列 : {word}')
+# 解答2：重複する文字を各1つだけ拾うバージョン　→　str_1 = "banana"・str_2 = "an"なら出力が「an」になる
+common_chars = ""
+str_1 = input("1つ目の文字列を入力してください > ")
+str_2 = input("2つ目の文字列を入力してください > ")
+for i in str_1:
+    if i in str_2 and i not in common_chars:    # iがstr_2に含まれる、かつ、common_charsには含まれない場合、
+        common_chars += i
+    elif i in str_2 and i in common_chars:    # iがstr_2に含まれる、かつ、common_charsにも含まれる場合、
+        continue
+print(f'重複する文字列 : {common_chars}')
