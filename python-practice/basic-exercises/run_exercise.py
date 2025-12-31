18b63073c9d5c6a1de8e72881b6d01e3d279912f
kubota-a <code199312@gmail.com>
2025-12-31 13:49:57 +0900

    udemy100問中29～31問目までを追加（昼）

--- python-practice/basic-exercises/run_exercise.py
+++ python-practice/basic-exercises/run_exercise.py
@@ -1,7 +1,19 @@
-l = [1, 5, 3, 2, 4]
-l_int = [int(i) for i in l]
-max_num = l_int[0]
-for num in l_int:
-    if num > max_num:
-        max_num = num
-print(f'リスト内の最大値 : {max_num}')
+
+
+# 再チャレンジ
+# 設計メモ
+# ※min()関数の戻り値は最小値そのものだから、最後にwords_listの文字列を出力させようとしても無理だった
+# ※なのでとりあえず関数なしで解いてみる
+# ```
+# ['Python', 'Ruby', 'PHP', 'JavaScript']を要素とするリストwords_listを宣言
+# 1番短い単語をとりあえず先頭の要素であると仮定するため、変数shortest_wordにwords_listのインデックス0に該当する要素を格納
+# for文で、words_listから「インデックス0以外の要素」を1つずつ取り出して変数wに格納する
+# もしwの文字列の長さが、shortest_wordの文字列の長さよりも短い場合は、shortest_wordをwで更新する
+# ループ終了後、「一番短い単語 : 」とshortest_wordをprintで出力
+
+words_list = ['Python', 'Ruby', 'PHP', 'JavaScript']
+shortest_word = words_list[0]
+for w in words_list[1:]:
+    if len(w) < len(shortest_word):
+        shortest_word = w
+print(f'一番短い単語 : {shortest_word}')
