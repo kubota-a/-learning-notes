e72e9c7c04dfd1248c35e4b22fafa502a35d04d6
kubota-a <code199312@gmail.com>
2026-01-01 10:46:22 +0900

    （2周目）udemy100問中23～25問目を追加（朝）

--- python-practice/basic-exercises/run_exercise.py
+++ python-practice/basic-exercises/run_exercise.py
@@ -1,18 +1,24 @@
-# 問題22
 # 設計メモ
-# ※2つ目の文字列をforループまわし、1つ目の文字列に含まれるかを判定
+# ※
 # ```
-# input()関数で「1つ目の文字列を入力してください > 」、受け取った文字列を変数words_1に格納
-# input()関数で「2つ目の文字列を入力してください > 」、受け取った文字列を変数words_2に格納 
-# 空の文字列duplicate_charsを宣言
-# for文でwords_2をまわして1文字ずつ取り出し、変数wに格納
-# もしwがwords_1に含まれる、かつ、duplicate_charsに含まれない場合は、duplicate_charsを「duplicate_chars+w」で更新
+# input()関数で「1つ目の文字列を入力してください > 」、受け取った文字列を変数wordsに格納
 # ループ終了後、「重複する文字列 : 」とduplicate_charsをprintで出力
 
-words_1 = input("1つ目の文字列を入力してください > ")
-words_2 = input("2つ目の文字列を入力してください > ")
-duplicate_chars = ""
-for w in words_2:
-    if w in words_1 and w not in duplicate_chars:    # andの後ろを「not in duplicate_chars:」だけで「w」を忘れてSyntaxError
-        duplicate_chars += w
-print(f'重複する文字列 : {duplicate_chars}')
+
+
+
+# データ増えてもいけるバージョンのリベンジ
+
+# 空のリストword_listを宣言
+# for文でrange(3)から「0,1,2」と1つずつ数を取り出し、変数iに格納
+# input()関数で「{i+1}つ目の英単語を入力してください > 」、受け取った文字列をword_listに追加
+# ループ終了後、word_listをsort()メソッドでアルファベット順に並べ替える
+# word_listをjoinメソッドでカンマ+半角スペース区切りの文字列に変換し、変数wordsに格納
+# 「並び替えた英単語 : 」とwordsをprintで出力
+
+word_list = []
+for i in range(3):
+    word_list.append(input(f'{i+1}つ目の英単語を入力してください > '))
+word_list.sort()
+words = ", ".join(word_list)
+print(f'並び替えた英単語 : {words}')
