0f098b6d9a8c41938135d2eaf8c6054b0df812b3
kubota-a <code199312@gmail.com>
2026-01-02 00:13:49 +0900

    1/1の雑感、解いた問題を更新

--- python-practice/basic-exercises/run_exercise.py
+++ python-practice/basic-exercises/run_exercise.py
@@ -17,19 +17,12 @@
 # 共通する値を格納したリスト : ['Python', 'Ruby']
 # ```
 
-# 設計メモ
-# ※l1をforループでまわして、要素1つずつl2に含まれているかを判定していくことにする
-# ```
-# 2つのリスト、l1とl2を宣言
-# 空のリストduplicate_wordsを宣言
-# for文でl1の要素を1つずつ取り出し、変数wに格納
-# wがl2に含まれていて、かつ、wがduplicate_wordsに含まれない場合は、wを要素としてduplicate_wordsに追加
-# ループ終了後、「共通する値を格納したリスト : 」とduplicate_wordsをprintで出力
-
+# 模範解答
 l1 = ['Python', 'Ruby', 'PHP', 'JavaScript']
 l2 = ['Java', 'Ruby', 'Golang', 'Python', 'TypeScript']
 duplicate_words =[]
-for w in l1:
-    if w in l2 and w not in duplicate_words:
-        duplicate_words.append(w)
+for w1 in l1:
+    for w2 in l2:
+        if w1 == w2 and w1 not in duplicate_words:
+            duplicate_words.append(w1)
 print(f'共通する値を格納したリスト : {duplicate_words}')
