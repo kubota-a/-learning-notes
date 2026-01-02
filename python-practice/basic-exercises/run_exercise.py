f2490a170f66a6f10ac8b65d63f95e6a3bfa0957
kubota-a <code199312@gmail.com>
2026-01-02 12:01:33 +0900

    udemy100問中、34～35問目を追加（朝）

--- python-practice/basic-exercises/run_exercise.py
+++ python-practice/basic-exercises/run_exercise.py
@@ -1,28 +1,33 @@
 # 設計メモ
 # ※
 # ```
-# input()関数で「1つ目の文字列を入力してください > 」、受け取った文字列を変数wordsに格納
-# ループ終了後、「重複する文字列 : 」とduplicate_charsをprintで出力
+# ['1', 2, '3', 4, '5', 6, '7', 8, '9', 10]を要素とするリストlを宣言
+# ループ終了後、「重複する文字列 : 」と　　　をprintで出力
 
 
-# 問題33
-# 2つのリストで共通の文字列を格納したリストを出力するプログラムを作成してください。
-# ※使用するリスト :
+# 問題35
+# 以下のリストで「整数型(int)の要素」だけを出力するプログラムを作成してください。
+# ※使用するリスト : 
 # ```
-# l1 = ['Python', 'Ruby', 'PHP', 'JavaScript']
-# l2 = ['Java', 'Ruby', 'Golang', 'Python', 'TypeScript']
+# l = ['1', 2, '3', 4.0, '5', 6, '7', 8.0, '9', 10]
 # ```
 # ▼期待する出力
 # ```
-# 共通する値を格納したリスト : ['Python', 'Ruby']
+# 整数型に絞り込んだリスト : [2, 6, 10]
 # ```
 
-# 模範解答
-l1 = ['Python', 'Ruby', 'PHP', 'JavaScript']
-l2 = ['Java', 'Ruby', 'Golang', 'Python', 'TypeScript']
-duplicate_words =[]
-for w1 in l1:
-    for w2 in l2:
-        if w1 == w2 and w1 not in duplicate_words:
-            duplicate_words.append(w1)
-print(f'共通する値を格納したリスト : {duplicate_words}')
+# 設計メモ
+# ※リストの内包表記でいけそう
+# ```
+# ['1', 2, '3', 4.0, '5', 6, '7', 8.0, '9', 10]を要素とするリストlを宣言
+# 新しく作成するリストnew_lに、lの各要素の中のint型のものだけ抜き出して格納
+#「整数型に絞り込んだリスト : 」とnew_lをprintで出力
+
+l = ['1', 2, '3', 4.0, '5', 6, '7', 8.0, '9', 10]
+new_l = [i for i in l if type(i) == int]
+print(f'整数型に絞り込んだリスト : {new_l}')
+
+# もっとスマートなコード
+l = ['1', 2, '3', 4.0, '5', 6, '7', 8.0, '9', 10]
+new_l = [i for i in l if isinstance(i, int)]
+print(f'整数型に絞り込んだリスト : {new_l}')
