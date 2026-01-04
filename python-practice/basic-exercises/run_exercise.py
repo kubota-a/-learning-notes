735f4585213b9f25b374b1988a64f25f54d6089b
kubota-a <code199312@gmail.com>
2026-01-04 22:54:48 +0900

    udemy100問中、42問目を追加

--- python-practice/basic-exercises/run_exercise.py
+++ python-practice/basic-exercises/run_exercise.py
@@ -5,7 +5,31 @@
 # ループ終了後、「重複する文字列 : 」と　　　をprintで出力
 
 
+# 再チャレンジ
+l = [1, 2, 3, 3]
+l_copy =[i for i in l]
+for i in l:
+    if i in l_copy:
+        l_copy.remove(i)
+print(len(l_copy))
+if len(l_copy) > 0:
+    print("重複している値があります。")
+else:
+    print("重複する値がありません。") 
 
-i = 1.5
-int(i)
-print(int(i), i)
+
+l = [1, 2, 3, 3]
+
+seen = []
+duplicate = False
+
+for i in l:
+    if i in seen:
+        duplicate = True
+        break
+    seen.append(i)
+
+if duplicate:
+    print("重複している値があります。")
+else:
+    print("重複する値がありません。")
