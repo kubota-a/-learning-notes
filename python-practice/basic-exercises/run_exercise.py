56b84ffbc4357e57f965a7874d94f1b2b58e22c7
kubota-a <code199312@gmail.com>
2026-01-06 22:59:53 +0900

    2周目）udemy100問中26～30問目を追加

--- python-practice/basic-exercises/run_exercise.py
+++ python-practice/basic-exercises/run_exercise.py
@@ -4,32 +4,6 @@
 # ['1', 2, '3', 4, '5', 6, '7', 8, '9', 10]を要素とするリストlを宣言
 # ループ終了後、「重複する文字列 : 」と　　　をprintで出力
 
-
-# 再チャレンジ
-l = [1, 2, 3, 3]
-l_copy =[i for i in l]
-for i in l:
-    if i in l_copy:
-        l_copy.remove(i)
-print(len(l_copy))
-if len(l_copy) > 0:
-    print("重複している値があります。")
-else:
-    print("重複する値がありません。") 
-
-
-l = [1, 2, 3, 3]
-
-seen = []
-duplicate = False
-
-for i in l:
-    if i in seen:
-        duplicate = True
-        break
-    seen.append(i)
-
-if duplicate:
-    print("重複している値があります。")
-else:
-    print("重複する値がありません。")
+# set使うバージョン
+l = [1, 2, 2, 3, 3, 4, 5]
+new_l = [i for i in l if i not in new_l]
