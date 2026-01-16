243c83591636848aab1720f72847bb355e233b55
kubota-a <code199312@gmail.com>
2026-01-16 17:31:49 +0900

    ルーティングを追加

--- flask-practice/app.py
+++ flask-practice/app.py
@@ -3,8 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return "Home page"
+
+@app.route("/about")
+def about():
+    return "About page"
+
+@app.route("/hello")
 def hello():
-    return "Hello Flask!"
+    return "Hello Flask"
 
 if __name__ == "__main__":
     app.run(debug=True)
