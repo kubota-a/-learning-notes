4946fc8dffded5f7d58a7770df0ea05398f63a8b
kubota-a <code199312@gmail.com>
2026-01-17 18:15:05 +0900

    Python側のメモリストを画面表示できるようにした

--- flask-practice/app.py
+++ flask-practice/app.py
@@ -3,9 +3,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
+memo_list = [
+    {'title': "test01", 'body': "チャッピーです。"},
+    {'title': "test02", 'body': "GPTです。"}
+]
+
+
 @app.route("/")
 def top():
-    return render_template('index.html')
+    return render_template('index.html', memo_list=memo_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
