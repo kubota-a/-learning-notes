9d232ff3c9d51793fa9e88fb9478a05f71b884a9
kubota-a <code199312@gmail.com>
2026-01-18 23:10:05 +0900

    メモアプリでDBを使えるように設定

--- flask-practice/app.py
+++ flask-practice/app.py
@@ -1,7 +1,31 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os  # ファイルパスの扱いに便利
 
 app = Flask(__name__)
+
+# =========================================
+# データベース
+# =========================================
+# プロジェクトフォルダ内にDBを作る設定
+basedir = os.path.abspath(os.path.dirname(__file__))    # ベースディレクトリ設定
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.db")    # SQLAlchemyの設定
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False    # パフォーマンストラッキングを無効化 (推奨)
+
+# SQLAlchemyとMigrateのインスタンスを作成
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# DBモデルを定義
+class Memo(db.Model):
+    __tablename__ = "memo"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+
 
 memo_list = [
     {'title': "test01", 'body': "チャッピーです。"},
