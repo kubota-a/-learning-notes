512806c3910bb953a4eb0f9041a9189cf47959d0
kubota-a <code199312@gmail.com>
2026-01-23 23:51:58 +0900

    新規登録画面のフォームのPOST部分を追加

--- flask-practice/app.py
+++ flask-practice/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, request, redirect, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os  # ファイルパスの扱いに便利
@@ -21,9 +21,9 @@ migrate = Migrate(app, db)
 class Memo(db.Model):
     __tablename__ = "memo"
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Text, nullable=False)
-    body = db.Column(db.Text, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)    # DBテーブルにid列を設置
+    title = db.Column(db.Text, nullable=False)    # DBテーブルにtitle列を設置
+    body = db.Column(db.Text, nullable=False)    # DBテーブルにbody列を設置
 
 
 # ルーティング定義
@@ -38,6 +38,20 @@ def top():
 # 新規登録画面
 @app.route("/regist", methods = ['GET', 'POST'])    # 新規登録画面にアクセスされたら下の関数が動く。GETでもPOSTでも下の関数で処理する。
 def regist():
+    if request.method == 'POST':    # 今来たリクエストがPOST（登録ボタンが押された）なら、下記の処理を行う
+        # フォームから値を取得
+        title = request.form.get('title')    # フォームからPOSTされたデータ（request.form）から、name="title" の値を取得して変数titlreに格納
+        body = request.form.get('body')    # フォームからPOSTされたデータ（request.form）から、name="body" の値を取得して変数bodyに格納
+
+        # SQLAlchemyでCreate
+        memo = Memo(title=title, body=body)
+        db.session.add(memo)    # 上記のデータを保存候補に入れる（まだDBに書きこまれてない）
+        db.session.commit()    # 保存候補になっていた処理を確定する（DBに書き込みされる）
+
+        # 一覧へ（PRGパターン）
+        return redirect("/")    # 登録が終わったら、トップページに戻す（GET）    
+    
+    # GET（画面を最初に開いたとき）だった場合は画面表示のみ
     return render_template('regist.html')
 
 
